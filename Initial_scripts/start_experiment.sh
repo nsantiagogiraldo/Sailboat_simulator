9a23f3fb38542b4cb8ad8f88e00d0a6854b291e3
Nelson <giraldobustamante@gmail.com>
2022-02-21 12:39:58 -0500

    21 feb

--- Initial_scripts/start_experiment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd
-gnome-terminal -e "bash -c \"source Velero.sh; exec bash\""
-gnome-terminal -e "bash -c \"source Puerto_serie.sh; exec bash\""
-source Anaconda.sh
-cd Documentos/Ubuntu_master/SNN_Codes/Spiking_codes/
-python Experiments.py
