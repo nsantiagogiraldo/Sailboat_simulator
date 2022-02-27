ea33e05fdd5d1e81ec75924489781a49793ca8ff
Nelson <giraldobustamante@gmail.com>
2022-02-26 19:30:04 -0500

    26_2 feb

--- Initial_scripts/Puerto_serie.sh
+++ Initial_scripts/Puerto_serie.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd home/nelson/Documentos/Ubuntu_master/SNN_Codes/Spiking_codes && socat pty,link=interface_1,raw,echo=1 pty,link=interface_2,raw,echo=1
+cd /home/nelson/Documentos/Ubuntu_master/SNN_Codes/Spiking_codes && socat pty,link=interface_1,raw,echo=1 pty,link=interface_2,raw,echo=1
--- SNN_Codes/Spiking_codes/Experiments.py
+++ SNN_Codes/Spiking_codes/Experiments.py
@@ -8,8 +8,9 @@ Created on Mon Feb  7 15:54:21 2022
 
 import SNN_train_controller as ctrl
 
+fail = False
 m = ctrl.SNN_complete_train_test()
-fail = m.train_SNN()
-m = ctrl.SNN_complete_train_test()
+#fail = m.train_SNN()
+#m = ctrl.SNN_complete_train_test()
 if not fail:
     m.test_SNN()
