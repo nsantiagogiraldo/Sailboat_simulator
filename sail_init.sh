8a2e3daa3bb909e4a13b3a326d6dd1321bba6d78
Nelson <giraldobustamante@gmail.com>
2022-09-11 18:12:37 -0500

    11 sep

--- sail_init.sh
+++ sail_init.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 source /opt/ros/kinetic/setup.bash
 source ~/catkin_ws/install_isolated/setup.bash
-source $DIR/Velero.sh &
+source $DIR/sailboat.sh &
 sleep 20
 rosservice call gazebo/unpause_physics
 
--- start_experiment.sh
+++ start_experiment.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
+echo $DIR > ~/.ros/path_read.txt
 gnome-terminal -e "bash -c \"cd $DIR; source sail_init.sh; exec bash\""
-gnome-terminal -e "bash -c \"cd $DIR;source Puerto_serie.sh; exec bash\""
+gnome-terminal -e "bash -c \"cd $DIR;source serial_port.sh; exec bash\""
 source $DIR/Anaconda.sh
 sleep 20
-python $DIR/SNN_Codes/Spiking_codes/Experiments.py
+python $DIR/SNN_Codes/Spiking_codes/Experiments.py  
 pkill terminal
